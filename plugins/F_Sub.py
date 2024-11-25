6c69e049b213f187fdf61592d65d5c6afb73baea
superpasses <165167006+superpasses@users.noreply.github.com>
2024-11-25 07:35:11 +0530

    Update F_Sub.py

--- plugins/F_Sub.py
+++ plugins/F_Sub.py
@@ -1,6 +1,6 @@
 """
 Apache License 2.0
-Copyright (c) 2022 @LazyDeveloper
+Copyright (c) 2024 @DramaMob
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -16,7 +16,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-Telegram Link : https://t.me/LazyDeveloper 
+Telegram Link : https://t.me/DramaMob 
 Repo Link : https://github.com/LazyDeveloperr/Gangster-Baby-Renamer-BOT
 License Link : https://github.com/LazyDeveloperr/Gangster-Baby-Renamer-BOT/blob/main/LICENSE
 """
